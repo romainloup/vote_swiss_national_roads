8652d97c8ddef365e8b2e106775a01d985ce5520
romainloup <romain.loup@unil.ch>
2024-11-13 14:10:19 +0100

    save changes

--- autoroutes_read_votes.R
+++ autoroutes_read_votes.R
@@ -4,19 +4,21 @@ library(dplyr)
 
 # https://dam-api.bfs.admin.ch/hub/api/dam/assets/32588879/master
 # Charger le fichier JSON
-file_path <- "/Users/rloup/Downloads/sd-t-17-02-20240922-eidgAbstimmung.json"
-json_data <- fromJSON(file_path, flatten = TRUE)
+file_path = "/Users/rloup/Downloads/sd-t-17-02-20240922-eidgAbstimmung.json"
+json_data = fromJSON(file_path, flatten = TRUE)
 
 # Extraire les informations au niveau de la Suisse
-swiss_data <- json_data$schweiz$vorlagen %>% 
-  bind_rows() %>% 
-  select(vorlagenId, vorlagenTitel.text, resultat.jaStimmenInProzent, resultat.neinStimmenAbsolut,
-         resultat.stimmbeteiligungInProzent, resultat.eingelegteStimmzettel, resultat.gueltigeStimmen)
+swiss_data = json_data$schweiz$vorlagen %>% 
+  bind_rows() 
+# %>% 
+#   select(vorlagenId, vorlagenTitel.text, resultat.jaStimmenInProzent, resultat.neinStimmenAbsolut,
+#          resultat.stimmbeteiligungInProzent, resultat.eingelegteStimmzettel, resultat.gueltigeStimmen)
 
-test = c()
+# Assembler les données par commune
+swiss_data_muni = c()
 for (i in 1:26) {
-  test = rbind(test, (swiss_data[[11]][[1]])[[4]][[i]])
+  swiss_data_muni = rbind(test, (swiss_data[[11]][[1]])[[4]][[i]])
 }
-test = rbind((swiss_data[[11]][[1:26]])[[4]][[1]])
 # Afficher le data frame résultant
-print(swiss_data)
+View(swiss_data_muni)
+dim(swiss_data_muni)
---
+++ map_autoroutes.R
@@ -0,0 +1,80 @@
+# autoroutes
+
+
+min_time_to_highway = read.csv("/Users/rloup/Desktop/autoroutes/min_time_to_highway3.csv")
+
+
+min_time_to_highway$t_min = apply(min_time_to_highway[,2:dim(min_time_to_highway)[2]], 1, FUN = min)
+
+View(min_time_to_highway)
+
+pal = leaflet::colorNumeric(
+  palette = "Reds",
+  domain = round(min_time_to_highway$t_min/60),
+  na.color = "#eeeeee")
+
+# label for hover
+labels3 = paste0(ch$NAME, ": ", round(min_time_to_highway$t_min/60), " min", collapse = NULL)
+
+leaflet(ch) %>%
+  # addProviderTiles("Stamen.TerrainBackground") %>%
+  # addProviderTiles("Esri.WorldShadedRelief") %>%
+  addProviderTiles("Esri.WorldTerrain") %>%
+  # addProviderTiles(providers$CartoDB.Positron) %>%
+  # addProviderTiles("Esri.WorldGrayCanvas") %>%
+  # addProviderTiles("Stadia.StamenTonerLabels") %>%
+  setView( lat=46.637785, lng=8.2 , zoom=7) %>%
+  # commune polygons
+  addPolygons(
+    fillColor = ~pal(round(min_time_to_highway$t_min/60)),
+    fillOpacity = 0.6,
+    color = "white",
+    weight = 0.1,
+    opacity = 1,
+    
+    highlight = highlightOptions(
+      weight = 2,
+      color = "#666",
+      fillOpacity = 0.7,
+      bringToFront = TRUE,
+      sendToBack = TRUE),
+    
+    label = labels3,
+    smoothFactor = 0.5
+  ) %>%
+  # canton polygons
+  addPolygons(data=ch_canton,
+              fill = FALSE,
+              color = "#666",
+              weight = 1,
+              opacity = 1,
+              smoothFactor = 0.5,
+              highlight = highlightOptions(
+                weight = 3,
+                color = "#666",
+                bringToFront = TRUE)
+  ) %>%
+  # addLegend(pal = factpal, values = ~language, opacity = 0.7, title = NULL,
+  #           position = "bottomright")
+  addScaleBar(position = "bottomright", options = scaleBarOptions(imperial = F)) %>%
+  addLegend(pal = pal, values = round(min_time_to_highway$t_min/60), opacity = 0.7, title = "Time",
+            position = "bottomright",
+            labFormat = labelFormat(transform = function(x) sort(x, decreasing = F))) %>%
+  addPolygons(data = lakes,
+              weight = 1,
+              fillColor = "#dddddd",
+              fillOpacity = 0.9,
+              color = "white",
+              highlight = highlightOptions(
+                weight = 1,
+                color = "#666",
+                fillOpacity = 1,
+                bringToFront = TRUE),
+              label = lakes$NAME,
+              labelOptions = labelOptions(
+                style = list(
+                  "color" = "#666",
+                  "font-style" = "italic"
+                )),
+              smoothFactor = 0.2) %>%
+  addProviderTiles("Stadia.StamenTerrainLines")
